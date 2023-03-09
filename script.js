776543d30d0d4827ce612634ec7c77d0e74542f5
saritha174 <peddisaritha174@gmail.com>
2023-03-09 11:08:03 +0530

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+function validateEmail(){
+    const email = document.getElementById('email-input');
+
+    //Regular expression for email validation
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(regex.test(email.value)){
+        alert("Valid email address");
+        email.vale = ""
+    }
+    else{
+        alert("Invalid email id please enter valid email id");
+        email.value=""
+    }
+}
